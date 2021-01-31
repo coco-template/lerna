37aece2212ab0feda03add5dde836e38beec7817
huang.jian <hjj491229492@hotmail.com>
2021-01-31 17:53:53 +0800

    feat: setup coco skyfall package

---
+++ packages/coco-skyfall/src/nest/mirror.ts
@@ -0,0 +1,15 @@
+// package
+import { css } from '@linaria/core';
+
+export const layout = css`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const header = css`
+  height: 30px;
+`;
+
+export const content = css`
+  flex: 1;
+`;
